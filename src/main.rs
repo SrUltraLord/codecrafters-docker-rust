64ca0b8a39fc2577c158cdd13e85ac17ccab01c0
SrUltraLord <dareyes6@espe.edu.ec>
2023-02-26 22:14:39 -0500

    Completed 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,31 @@
 use anyhow::{Context, Result};
+use std::{
+    io::{self, Write},
+    process,
+};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage!
     let args: Vec<_> = std::env::args().collect();
-    let command = &args[3];
-    let command_args = &args[4..];
-    let output = std::process::Command::new(command)
-        .args(command_args)
+
+    let piped_command = &args[3];
+    let piped_command_args = &args[4..];
+    let output = process::Command::new(piped_command)
+        .args(piped_command_args)
         .output()
         .with_context(|| {
             format!(
                 "Tried to run '{}' with arguments {:?}",
-                command, command_args
+                piped_command, piped_command_args
             )
         })?;
 
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
-    } else {
-        std::process::exit(1);
+    if !output.status.success() {
+        process::exit(1);
     }
+
+    io::stdout().write_all(&output.stdout)?;
+    io::stderr().write_all(&output.stderr)?;
 
     Ok(())
 }
